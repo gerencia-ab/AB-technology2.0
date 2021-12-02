9509e681e4c24a588a41f27b0a10774eb1095e54
AB-Technology <kevinenriquejs@ufps.edu.co>
2021-12-02 16:09:18 -0500

    Feat - Seccion de equipo, Fix - Seccion Quienes somos

---
+++ Views/equipo.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+        include_once '../php/scripts/scriptsCSS.php';
+    ?>
+</head>
+    <body>
+        
+        <?php
+            include_once '../php/header.php';
+        ?>
+
+
+
+    <div class="container" >
+        <div class="row" >
+            <div style="margin-top: 100px;"></div>
+            <h2 class="blog-titulo" style="color: #FFFFFF;">Nuestro Equipo</h2>
+            <div class="col xs sm md lg quienes-somos">
+                <h2 class="blog-titulo" style="margin-bottom: 25px">JOSE JAVIER CASTRO MEDINA</h2>
+                <p style="text-align: justify;">
+                    Instagram: @javiercastrom1	
+                </p>
+                <p style="text-align: justify;">
+                    Correo: jcastro@ab-sistemas.com
+                </p>
+                <p style="text-align: justify;">
+                    Cofundador de AB TECHNOLOGY GROUP
+                </p>
+                <p style="text-align: justify;">
+                    Area de Marketing digital
+                </p>
+                
+                <lottie-player src="https://assets10.lottiefiles.com/packages/lf20_kmabq1m3.json"  background="transparent"  speed="1"  style="width: 100%; height: 300px;"  loop autoplay></lottie-player>
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modal-javier">
+                    Ver Perfil
+                </button>
+            </div>
+        </div>
+        <!-- Modal -->
+        <div class="modal fade" id="modal-javier" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Perfil</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                        <div class="modal-body">
+                            <p style="text-align: justify;">
+                                Nací en la ciudad de Bogotá en el año 1989 y desde niño resido en la ciudad de 
+                                Cúcuta. Me gradué de bachiller en el Gimnasio Domingo Savio en el año 2006, luego 
+                                de haber cursado allí todos mis estudios de Primaria y Bachillerato.
+                            </p>
+                            <p style="text-align: justify;">
+                                Posteriormente adelanté mis estudios presencialmente en Administración de Empresas, 
+                                en la UNAB - Universidad autónoma de Bucaramanga, lo que me formó como un ciudadano 
+                                competente académicamente, con sólida formación ética, respetuoso de los valores 
+                                fundamentales del desarrollo humano, capacitándome para contribuir al mejoramiento 
+                                de la sociedad en áreas de la Administración en los sectores público y privado. 
+                            </p>
+                            <p style="text-align: justify;">
+                                Recibí mi título profesional como Administrador de Empresas en diciembre de 2013, 
+                                en la ciudad de Bucaramanga.
+                            </p>
+                            <p style="text-align: justify;">
+                                Del año 2014 al 2019 laboré como  Contratista independiente en la Corporación autónoma 
+                                regional de la Frontera Nororiental – CORPONOR en la ciudad de Cúcuta mediante ordenes 
+                                de prestación de servicios cuyo objeto principal fue: Apoyo a la subdirección Financiera, 
+                                lo que me permitió adquirir una perspectiva global del área administrativa de la Corporación. 
+                            </p>
+                            <p style="text-align: justify;">
+                                Durante esta etapa realicé una especialización en Gerencia Estratégica con la “Universidad 
+                                De La Sabana” mediante convenio con la Cámara de Comercio de Cúcuta, obteniendo el titulo 
+                                de Especialista en Gerencia Estratégica en noviembre de 2017. Esto me fortaleció en el 
+                                conocimiento propio del area de estrategia, con énfasis en los procesos de análisis y 
+                                direccionamiento estratégico, implementación y evaluación de la estrategia empresarial.  
+                            </p>
+                            <p style="text-align: justify;">
+                                Desde 2019 a la fecha me he dedicado al marketing digital como cofundador de  una emprendimiento 
+                                con una empresa de desarrollo de software denominada AB Technology, aplicando estrategias de 
+                                comercialización en los medios digitales, trabajando en mejorar la visibilidad de nuestros 
+                                clientes en las redes sociales, mediante la comercialización, comunicación, exposición y ofrecimiento 
+                                de  un servicio o un  producto que logren la venta de una forma más directa. 
+                            </p>
+                            <p style="text-align: justify;">
+                                El término Marketing digital es usado para resumir todos tus esfuerzos de marketing en el ambiente 
+                                online.
+                            </p>
+                            <p style="text-align: justify;">
+                                Complementé mis estudios con un curso virtual de 40 horas en Google Actívate con examen de certificación 
+                                del 09/11/2020, y un Diplomado presencial  en “Estrategias Comerciales en el Marketing de Negocios 
+                                Internacionales” en la Fundación de Estudios Superiores FESC de 120 horas en julio de 2019. Ejercí 
+                                como Community Manager de “Cúcuta Motors” concesionario de vehículos Toyota, a finales de 2019.
+                            </p>
+                            <p style="text-align: justify;">
+                                A lo largo de mi formación, he potenciado mi espíritu de trabajo en equipo, iniciativa e implicación en 
+                                las tareas.
+                            </p>
+                            <p style="text-align: justify;">
+                                Mi objetivo principal es conseguir mediante diversas estrategias y técnicas de marketing, que el consumidor 
+                                que haya adquirido con anterioridad alguno de nuestros productos o servicios, nos siga comprando y se convierta
+                                en un cliente habitual. - Un cliente satisfecho y fidelizado es la mejor comunicación que pueda tener una 
+                                empresa.
+                            </p>
+                            <p style="text-align: justify;">
+                                El marketing digital representa la evolución del marketing tradicional, y el marketing por el que deberían apostar 
+                                las empresas si no quieren perder la oportunidad de subirse al tren del futuro. Además cuenta con muchos medios 
+                                que logran transmitir un mensaje a millones de personas, con solo una publicación en un blog, en redes sociales 
+                                o a través de una campaña de email marketing.
+                            </p>
+                            <p style="text-align: justify;">
+                                Cada vez más los influencers digitales nos hemos consolidado como asociados estratégicos de las empresas para 
+                                aumentar su alcance y visibilidad online.
+                            </p>
+                            <p style="text-align: justify;">
+                                En el Marketing Digital, la viralidad es la capacidad que tiene un contenido, difundido en Internet, de compartirse 
+                                o recibir muchas visitas en un período corto del tiempo.
+                            </p>
+                            <p style="text-align: justify;">
+                                Hoy mismo las pequeñas marcas pueden hacer buenas campañas y atraer consumidores del otro lado de la ciudad, 
+                                del país y del mundo.
+                            </p>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div>
+
+    <?php
+            include_once '../php/footer.php';    
+            include '../php/scripts/scriptsJS.php'
+        ?>    
+</body>
+
+
+
+</html>
